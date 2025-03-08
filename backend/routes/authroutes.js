d78bb82269849609181d5f2a66bb1db9c23ca948
Viraja <palletiviraja@gmail.com>
2025-03-08 19:58:27 +0530

    Create authroutes.js

---
+++ backend/routes/authroutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { registerPatient, loginUser, loginAdmin } from '../controllers/authControllers.js';
+
+const router = express.Router();
+
+router.post('/register/patient', registerPatient);
+
+router.post('/login/user', loginUser);
+
+router.post('/login/admin', loginAdmin);
+
+export default router;
